92d66837ae760ed7febbe30ea985f1d8560f2955
estebanmocho9-lab <estebanmocho9@gmail.com>
2026-08-31 06:41:11 -0300

    Improve product catalog and main product search flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import ArkonVisual from '../components/ArkonVisual';
 import ArkonAnalysisConnector from '../components/ArkonAnalysisConnector';
+import CatalogUiEnhancements from '../components/CatalogUiEnhancements';
 
 export default function Home() {
   return (
     <>
       <ArkonVisual />
       <ArkonAnalysisConnector />
+      <CatalogUiEnhancements />
     </>
   );
 }
